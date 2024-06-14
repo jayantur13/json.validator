72b2a0f798eb65685cabc06fb5eb5c8d92db4f2c
jayantur13 <jayantur13@gmail.com>
2024-06-14 19:51:04 +0530

    Feature Changes

--- README.md
+++ README.md
@@ -1,12 +1,35 @@
 # json.validator (WIP)
 
-Basic type of json validation tool to validate and format json data
+Basic type of json validation tool to validate and format, download json (jsontoxml) data
+
+# Features
+
+- Validate
+  > Green color if okay
+  > Red color if error
+- Basic error message to fix json
+- Option to download as file (appears after validate) in .json and .xml
+- Automatic data reset in 1 min (if focus lost on textarea)
+- Manual clear to clear/reset everything
+- Line numbers (Planned)
 
 # Open for
 
 - Contributions, enhancements etc
 - Support me (sponsor) or star the repo
 
+# Changes
+
+## 14-06-2024
+
+- Added basic formatting for json
+- Added option to download as a file (both as .json and .xml )
+- Auto data reset
+
+# Credit
+
+- json2xml via https://github.com/nashwaan/xml-js
+
 # License
 
-This project is licensed under the [MIT Licence](https://github.com/jayantur13/json.validator/blob/master/LICENSE.md)
+This project is licensed under the [MIT License](https://github.com/jayantur13/json.validator/blob/master/LICENSE.md)
--- js/script.js
+++ js/script.js
@@ -1,54 +1,146 @@
 const validateBtn = document.getElementById("submit-btn");
 const clearBtn = document.getElementById("clear-btn");
-let defaultErrMsg = "Watch this space for error!";
+const dlJsonBtn = document.getElementById("dl-json-btn");
+const dlXMLtn = document.getElementById("dl-xml-btn");
+let visible = document.getElementById("download-btn-dropdown");
+const jsonDataInput = document.getElementById("jsonDataArea");
+let defaultInfoMsg = "Watch this space for information!";
 
 const validateJSON = () => {
   const jsonDataInput = document.getElementById("jsonDataArea").value.trim();
-  let boolState;
-  if (jsonDataInput.length == 0 || jsonDataInput == " ") {
-    alert("Enter json data to validate");
-  } else {
-    try {
-      boolState = JSON.parse(jsonDataInput, null, 2);
-      if (boolState) {
-        document.getElementById("jsonDataArea").style.backgroundColor =
-          "#D1FFBD";
-        let errorMsg = document.getElementById("errorMsg");
-        if (errorMsg != "Watch this space for error!") {
-          document.getElementById("errorMsg").innerText = defaultErrMsg;
-        }
-      }
-    } catch (error) {
-      if (error) {
-        document.getElementById("jsonDataArea").style.backgroundColor =
-          "#FFF7F7";
-        document.getElementById("errorMsg").innerText = error;
-      }
+  let colorState = "pass";
+  try {
+    let jsonObject = JSON.parse(jsonDataInput);
+    const prettyJsonString = JSON.stringify(jsonObject, null, 2);
+    document.getElementById("jsonDataArea").value = prettyJsonString;
+    visible.style.display = "inline-block";
+    return colorState;
+  } catch (error) {
+    if (error) {
+      colorState = error;
+      return colorState;
     }
   }
 };
 
 const clearEntireThing = () => {
   let jsonDataInput = document.getElementById("jsonDataArea").value;
-  let errorMsg = document.getElementById("errorMsg").innerText;
+  let infoArea = document.getElementById("infoArea").innerText;
 
   if (
     jsonDataInput.length != 0 ||
-    (jsonDataInput != " " && errorMsg === "Watch this space for error!")
+    (jsonDataInput != " " && infoArea === "Watch this space for information!")
   ) {
     document.getElementById("jsonDataArea").value = "";
     document.getElementById("jsonDataArea").style.backgroundColor = "#FFFFFF";
+    document.getElementById("infoArea").style.background = "#FFFFFF";
   }
   if (
     jsonDataInput.length != 0 ||
-    (jsonDataInput != " " && errorMsg != "Watch this space for error!")
+    (jsonDataInput != " " && infoArea != "Watch this space for information!")
   ) {
     document.getElementById("jsonDataArea").value = "";
-    document.getElementById("errorMsg").innerText = defaultErrMsg;
+    document.getElementById("infoArea").innerText = defaultInfoMsg;
     document.getElementById("jsonDataArea").style.backgroundColor = "#FFFFFF";
+    document.getElementById("infoArea").style.background = "#FFFFFF";
+  }
+  visible.style.display = "none";
+};
+
+const checkThenWarn = () => {
+  const jsonDataInput = document.getElementById("jsonDataArea").value;
+  if (
+    jsonDataInput.length == 0 ||
+    jsonDataInput == "" ||
+    jsonDataInput == " "
+  ) {
+    alert("Enter json data to validate");
+  } else {
+    let res = validateJSON();
+    if (res != "pass") {
+      //error if not pass
+      document.getElementById("jsonDataArea").style.backgroundColor = "#FFF7F7";
+      document.getElementById("infoArea").style.backgroundColor = "#FFF7F7";
+      document.getElementById("infoArea").innerText = res;
+    } else {
+      //okay if pass
+      document.getElementById("jsonDataArea").style.backgroundColor = "#D1FFBD";
+      let infoArea = document.getElementById("infoArea");
+      if (infoArea != "Watch this space for information!") {
+        document.getElementById("infoArea").style.backgroundColor = "#D1FFBD";
+        document.getElementById("infoArea").innerText = "JSON is valid!";
+      }
+    }
   }
 };
 
-validateBtn.addEventListener("click", validateJSON);
+const downloadJSONFile = () => {
+  const jsonDataInput = document.getElementById("jsonDataArea").value;
+  if (
+    jsonDataInput.length == 0 ||
+    jsonDataInput == "" ||
+    jsonDataInput == " "
+  ) {
+    alert("Enter json data to validate");
+  } else {
+    const fileName = "download.json";
+
+    const blob = new Blob([jsonDataInput], { type: "text/json" });
+    const downloadLink = document.createElement("a");
+    downloadLink.href = URL.createObjectURL(blob);
+    downloadLink.download = fileName;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  }
+};
+
+const structJSONToXMLdownload = () => {
+  const jsonDataInput = document.getElementById("jsonDataArea").value;
+  if (
+    jsonDataInput.length == 0 ||
+    jsonDataInput == "" ||
+    jsonDataInput == " "
+  ) {
+    alert("Enter json data to validate");
+  } else {
+    const xml = json2xml(jsonDataInput, { compact: true, spaces: 4 });
+    const fileName = "download.xml";
+
+    const blob = new Blob([xml], { type: "text/xml" });
+    const downloadLink = document.createElement("a");
+    downloadLink.href = URL.createObjectURL(blob);
+    downloadLink.download = fileName;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  }
+};
+
+let cleanerTimeout;
+
+//Focus moved
+document.getElementById("jsonDataArea").addEventListener("blur", () => {
+  //Clear everything in 1 min
+  if (cleanerTimeout) {
+    clearTimeout(cleanerTimeout);
+  }
+  cleanerTimeout = setTimeout(clearEntireThing, 60000);
+});
+
+// Add focus
+document.getElementById("jsonDataArea").addEventListener("focus", () => {
+  // Clear the timeout if the input regains focus
+  if (cleanerTimeout) {
+    clearTimeout(cleanerTimeout);
+    cleanerTimeout = null; // Clear the timeout ID
+  }
+});
+
+validateBtn.addEventListener("click", checkThenWarn);
 
 clearBtn.addEventListener("click", clearEntireThing);
+
+dlJsonBtn.addEventListener("click", downloadJSONFile);
+
+dlXMLtn.addEventListener("click", structJSONToXMLdownload);
